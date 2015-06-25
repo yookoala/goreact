3340994c2c56c67b00e24402025b8ac9dd0b8fa2
Koala Yeung <koalay@gmail.com>
2015-06-25 16:04:34 +0800

    Fix property call in test javascripts

--- _test/js/main.js
+++ _test/js/main.js
@@ -3,6 +3,6 @@ var CommentBox = React.createClass({
     render: (function () {
         return React.createElement("div", {
             className: "commentBox"
-        }, "Hello, ", this.props.data, "! I am a CommentBox.");
+        }, "Hello, ", this.props.data.hello, "! I am a CommentBox.");
     })
 });
--- _test/js/main.jsx
+++ _test/js/main.jsx
@@ -3,7 +3,7 @@ var CommentBox = React.createClass({
 	render: function() {
 		return (
 			<div className="commentBox">
-				Hello, {this.props.data}! I am a CommentBox.
+				Hello, {this.props.data.hello}! I am a CommentBox.
 			</div>
 		);
 	}
