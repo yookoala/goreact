b0482ed5e24e901dc5373430940cd7827dcb3f0a
Koala Yeung <koalay@gmail.com>
2015-06-25 14:05:13 +0800

    Have the dummy index.html display something
    The index.html now renders the output correctly.

---
+++ _test/js/interface.jsx
@@ -0,0 +1,60 @@
+/**
+ *
+ */
+(function(React) {
+
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.props.data} />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+React.render(
+	<div>
+	  <h1>Hello, world! Hello</h1>
+	  <CommentBox data={data} />,
+  </div>,
+  document.getElementById('main')
+);
+
+})(React);
